3fd22f7e8209bb61c4287543906046940ecdd421
Gordon German <gordon.german@csiro.au>
2020-10-21 15:39:32 +0800

    testing auto credentials

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from gordon branch! You can edit <code>src/App.js</code> and save to reload.
+          Changed from gordon branch! You can edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
